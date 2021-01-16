8f7f71e41f1a77e92457944abb943fe8ab210ef7
LunarCoffee <lunarcoffee.pjc@gmail.com>
2021-01-16 02:17:28 -0500

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+import flask
+import opentok
+
+OPENTOK_API_KEY = "***REMOVED***"
+OPENTOK_API_SECRET = "***REMOVED***"
+
+api_opentok = opentok.OpenTok(OPENTOK_API_KEY, OPENTOK_API_SECRET)
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def test():
+    return "i despise existence with a fervent passion"
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", "8000"))
+    app.run(port=port)
